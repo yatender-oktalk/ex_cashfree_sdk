db582d9c21e84fe4123cd203cf030ace2dd530a7
Yatender Suman <yatender.nitk@gmail.com>
2024-08-04 19:28:49 +0200

    feat: :fire: Initialized the project for cashfree sdk

---
+++ lib/ex_cashfree_sdk.ex
@@ -0,0 +1,18 @@
+defmodule ExCashfreeSdk do
+  @moduledoc """
+  Documentation for `ExCashfreeSdk`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCashfreeSdk.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_cashfree_sdk_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCashfreeSdkTest do
+  use ExUnit.Case
+  doctest ExCashfreeSdk
+
+  test "greets the world" do
+    assert ExCashfreeSdk.hello() == :world
+  end
+end
